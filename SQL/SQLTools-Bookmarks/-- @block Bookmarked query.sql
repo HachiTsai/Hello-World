c315d2bbbe97a57d4c92dd05d93fbfd3a1cfaa83
hachi.tsai@gmail.com <hachi.tsai@gmail.com>
2025-07-27 14:32:40 +0800

    存到SQLTools bookmarks是選取文字後右鍵選bookmark selected query

---
+++ SQL/SQLTools-Bookmarks/-- @block Bookmarked query.sql
@@ -0,0 +1,5 @@
+-- @block Bookmarked query
+-- @group Price Checks
+-- @name price>1000
+
+SELECT * FROM products WHERE price > 1000;
--- SQL/search.sql
+++ SQL/search.sql
@@ -1,1 +1,4 @@
-SELECT * FROM products;
+-- 1. 先把下面這行文字選取起來
+SELECT * FROM products WHERE price > 1000;
+
+-- 2. 然後在選取的文字上按右鍵，就會看到選項了
