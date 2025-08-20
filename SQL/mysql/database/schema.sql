0d758ae197177a3100206e67e783dbaa40365419
hachi.tsai@gmail.com <hachi.tsai@gmail.com>
2025-08-20 22:20:26 +0800

    deleted files which no-using sqltools and did some practice of sql

--- SQL/mysql/database/schema.sql
+++ SQL/mysql/database/schema.sql
@@ -1,18 +1,37 @@
--- 建立資料表
+-- 建立資料表product
 
-/* CREATE TABLE IF NOT EXISTS users (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL,
-  email TEXT UNIQUE,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
- */ 
+-- CREATE TABLE IF NOT EXISTS product (
+--   id INTEGER PRIMARY KEY AUTOINCREMENT,
+--   name TEXT NOT NULL,
+--   price INTEGER NOT NULL,
+--   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
 
 -- 插入測試資料
---INSERT INTO users (name, age, email) VALUES 
---  ('Carol', 15, 'carol@example.com'),
---  ('Grid', 31, 'grid@example.com'),
---  ('Henry', 28, 'henry@example.com');
+-- INSERT INTO product (name, price) VALUES 
+--   ('car', 2000000),
+--   ('handphone', 30000),
+--   ('notebook', 35000);
+
+--select * from product where name = 'car'
+
+/* --------TABLE seperate-line -------- */
+
+
+-- 建立資料表users
+
+-- CREATE TABLE IF NOT EXISTS users (
+--   id INTEGER PRIMARY KEY AUTOINCREMENT,
+--   name TEXT NOT NULL,
+--   age TEXT UNIQUE,
+--   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+
+-- 插入測試資料
+--  INSERT INTO users(name, age, email) VALUES 
+--    ('william', 12, 'William@example.com');
 
 
 -- 基礎查詢練習 - /condition1/ + /condition2/  ; Like '%i%' / and / age/  (as a string ,it should be'string')
@@ -20,4 +39,6 @@
 
 -- 基礎查詢練習 - multiple choice "in"
 -- SELECT * FROM users WHERE age in (15, 31, 28) or name like '%i%'
+
+--SELECT * FROM users WHERE name like '%i%'
 
