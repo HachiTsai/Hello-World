999d2ad7d2ed41a7e453414ad8b4ecbec2e1eb43
Hachi.TSAI <hachi.tsai@gmail.com>
2025-08-20 16:27:45 +0800

    sqlite practice and created a workspace

--- SQL/mysql/database/schema.sql
+++ SQL/mysql/database/schema.sql
@@ -10,8 +10,14 @@
 
 -- 插入測試資料
 --INSERT INTO users (name, age, email) VALUES 
---  ('Alice', 25, 'alice@example.com'),
---  ('Bob', 30, 'bob@example.com');
+--  ('Carol', 15, 'carol@example.com'),
+--  ('Grid', 31, 'grid@example.com'),
+--  ('Henry', 28, 'henry@example.com');
 
--- 基礎查詢練習
-SELECT * FROM users WHERE age > 20;
+
+-- 基礎查詢練習 - /condition1/ + /condition2/  ; Like '%i%' / and / age/  (as a string ,it should be'string')
+-- SELECT * FROM users WHERE name like '%i%' and age >30
+
+-- 基礎查詢練習 - multiple choice "in"
+-- SELECT * FROM users WHERE age in (15, 31, 28) or name like '%i%'
+
