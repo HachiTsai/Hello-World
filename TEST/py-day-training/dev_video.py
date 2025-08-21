133dc90eabb43a20d87c7859b4ffd81d18612f28
hachi.tsai@gmail.com <hachi.tsai@gmail.com>
2025-08-21 22:09:26 +0800

    python practice for dataframe added

--- TEST/py-day-training/dev_video.py
+++ TEST/py-day-training/dev_video.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
+df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6], "C": [7, 8, 9], "D": [10, 11, 12]})
 # print(np.__version__) 
 
 df2 = pd.DataFrame({"C": [11, 12, 13], "D": [14, 15, 16]})
