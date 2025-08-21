769636da39eed3ddf7d56f2a1d1b2abcf793aa5a
Hachi.TSAI <hachi.tsai@gmail.com>
2025-08-21 15:30:30 +0800

    record the ipykernel error and its solutions, data named Error_fix.md at conda-management

--- TEST/py-day-training/dev_video.py
+++ TEST/py-day-training/dev_video.py
@@ -3,3 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
+# print(np.__version__) 
+
+df2 = pd.DataFrame({"C": [11, 12, 13], "D": [14, 15, 16]})
+print(df)
+print(df2)
