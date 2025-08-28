ba03e68f8ed05d614af82493817fc81c76e7ecf8
hachi.tsai@gmail.com <hachi.tsai@gmail.com>
2025-08-28 21:58:09 +0800

    sql time setting set to local time

--- SQL/mysql/database/schema.sql
+++ SQL/mysql/database/schema.sql
@@ -10,12 +10,36 @@
 
 -- 插入測試資料
 
-INSERT INTO Daily_report (content, reference) VALUES 
-  ('使用SIDERAI進行PDF解讀並轉檔py與json含RCMT-Tainan專案', 'z-my-note\hitachi-dcs\DDC-functionblock-detail'),
-  ('學習看P&ID圖', 'RCMT-Tainan尚未建學習檔'),
-  ('新增MLC1的SIC圖','loop.md/尚未完成');
+-- INSERT INTO Daily_report (content, reference, tag) VALUES 
+--   ('新增db的tag欄位，並更新欄位資訊', 'SQL/mysql/database/hi.db', 'sql/db/schema');
+-- SELECT * FROM Daily_report;
 
--- SELECT * FROM Daily_report WHERE content = '查詢pip中需要更新版本的套件';
+-- SELECT * FROM Daily_report WHERE content = 'pip';
+
+-- 模糊查詢
+-- SELECT * FROM Daily_report WHERE content like '%MLC%'; 
+
+-- 新增欄位tag，並設定預設值為'未分類'
+-- ALTER TABLE Daily_report ADD COLUMN tag TEXT DEFAULT '未分類';
+
+-- 更新現有資料（方法1）  
+--UPDATE Daily_report SET tag = '專案' WHERE content LIKE '%RCMT%';  
+
+--查詢結果  
+--SELECT * FROM Daily_report;  
+
+-- 將 id=1 的資料 tag 設為 '專案'（方法2）
+-- UPDATE Daily_report 
+-- SET tag = 'pip' 
+-- WHERE id = 1;
+
+
+
+-- 查詢資料表結構
+--PRAGMA table_info(Daily_report);
+
+
+
 
 /* --------TABLE seperate-line -------- */
 
@@ -63,3 +87,25 @@ INSERT INTO Daily_report (content, reference) VALUES
 
 --SELECT * FROM users WHERE name like '%i%'
 
+--  插入資料時轉換為本地時間
+-- INSERT INTO Daily_report (content, created_at, reference, tag)
+-- VALUES ('測試', datetime('now', 'localtime') , 'start of day', 'sql/time');
+
+--查詢時轉換為本地時間  
+-- SELECT datetime(created_at, 'localtime') FROM Daily_report; 
+
+-- 將所有資料的 created_at 更新為當前本地時間  
+-- UPDATE Daily_report   
+-- SET created_at = datetime('now', 'localtime');  
+
+-- 針對特定條件更新（例如只更新 id=1 的資料）  
+-- UPDATE Daily_report   
+-- SET created_at = datetime('now', 'localtime')   
+-- WHERE id = 1;  
+
+-- 1. 檢查原始資料（確認時間格式與錯誤）  
+-- SELECT id, content, created_at FROM Daily_report;  
+
+--  2. 執行時區修正（UTC → UTC+8）  
+-- UPDATE Daily_report  
+-- SET created_at = datetime(created_at, '+8 hours');  
